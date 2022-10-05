913ee1a807d88e02a214321accb776a6341af720
Attituding <84680760+Attituding@users.noreply.github.com>
2022-10-05 09:36:46 -0700

    feat: add territory scoring refactor: formatting

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -3,50 +3,45 @@ package driver;
 import java.util.Scanner;
 
 public class Driver {
+
     private static String padString(String string, int paddingLength) {
         String paddedString = string;
-        
+
         for (int i = string.length(); i < paddingLength + 1; i++) {
             paddedString += " ";
         }
-        
+
         return paddedString;
     }
-    
+
     private static int parseInt(String integer) {
         try {
             return Integer.parseInt(integer);
-        } catch (NumberFormatException e) {}
-        
+        } catch (NumberFormatException e) {
+        }
+
         return -1;
     }
-    
+
     private static void print(Go game) {
         int size = game.getSize();
         int paddingLength = Integer.toString(size).length();
-        
+
         String header = padString("0", paddingLength);
-        
+
         for (int i = 0; i < size; i++) {
             header += padString(i + 1 + " ", paddingLength);
         }
-        
-        System.out.println("Player " + game.getPlayer() + " - Move " + game.getMove() );
+
+        System.out.println("Player " + game.getPlayer() + " - Move " + game.getMove());
         System.out.println(header);
-        
+
         for (int i = 0; i < size; i++) {
             String rowString = padString(i + 1 + "", paddingLength);
-            
+
             for (int y = 0; y < size; y++) {
                 int rawValue = game.getValue(i, y);
-                
-                switch (rawValue) {
-                    case Go.EMPTY -> rowString += padString("·", paddingLength);
-                    case Go.BLACK -> rowString += padString("B", paddingLength);
-                    case Go.WHITE -> rowString += padString("W", paddingLength);
-                }
-                
-                /*
+
                 if (rawValue == Go.EMPTY) {
                     rowString += padString("·", paddingLength);
                 } else if (rawValue == Go.BLACK) {
@@ -54,67 +49,71 @@ public class Driver {
                 } else if (rawValue == Go.WHITE) {
                     rowString += padString("W", paddingLength);
                 }
-                */
             }
-            
+
             System.out.println(rowString);
         }
     }
-    
+
     public static void main(String[] args) {
         Go game = new Go(19, false);
-        
+
         Scanner input = new Scanner(System.in);
-        
+
         while (game.getGameEnded() == false) {
             print(game);
-            
+
             System.out.println("Do you want to skip your turn? (n/y/true/false)");
-            
+
             String line = input.next();
-            
+
             if (line.contains("y") || line.equals("true")) {
                 System.out.println("Skipping turn!");
                 game.pass();
                 continue;
             }
-            
+
             int row = -1;
             int column = -1;
             boolean firstTry = true;
-            
+
             do {
                 if (firstTry == false) {
                     System.out.println("Illegal move. Try again.");
                 }
-                
+
                 firstTry = false;
-                
+
                 System.out.println("Input a row number:");
                 row = parseInt(input.next()) - 1;
-                
+
                 while (game.isInBounds(row) == false) {
                     System.out.println("Invalid row. Try again:");
                     row = parseInt(input.next()) - 1;
                 }
-                
+
                 System.out.println("Input a column number:");
                 column = parseInt(input.next()) - 1;
-                
+
                 while (game.isInBounds(column) == false) {
                     System.out.println("Invalid column. Try again:");
                     column = parseInt(input.next()) - 1;
                 }
             } while (game.canPlay(row, column) == false);
-            
+
             game.play(row, column);
         }
 
         input.close();
-        
-        int blackScore = game.getScoreArea(Go.BLACK);
-        int whiteScore = game.getScoreArea(Go.WHITE);
-        
-        System.out.println("Both players skipped! Game over! Black got " + blackScore + " points and White got " + whiteScore + " points!");
+
+        int blackScoreArea = game.getScoreArea(Go.BLACK);
+        int whiteScoreArea = game.getScoreArea(Go.WHITE);
+
+        int blackScoreTerritory = game.getScoreTerritoy(Go.BLACK);
+        int whiteScoreTerritory = game.getScoreTerritoy(Go.WHITE);
+
+        System.out.println("Both players skipped! Game over!");
+        System.out.println("Area Scoring: Black got " + blackScoreArea + " points and White got " + whiteScoreArea + " points!");
+        System.out.println("Territory Scoring: Black got " + blackScoreTerritory + " points and White got " + whiteScoreTerritory + " points!");
     }
 }
--- src/driver/Go.java
+++ src/driver/Go.java
@@ -4,68 +4,80 @@ import java.util.ArrayList;
 import java.awt.Point;
 
 public class Go {
+
     public static final int EMPTY = 0;
     public static final int BLACK = 1;
     public static final int WHITE = 2;
-    
+
     private final int[][] board;
     private final int size;
     private final boolean allowSelfCapture;
-    
+
     private boolean lastPlayerPassed = false;
     private boolean gameEnded = false;
     private int move = 1;
     private int currentPlayer = BLACK;
-    
+    private int stonesCapturedByBlack = 0;
+    private int stonesCapturedByWhite = 0;
+
     public Go(int size, boolean allowSelfCapture) {
         this.board = new int[size][size];
         this.size = size;
         this.allowSelfCapture = allowSelfCapture;
-        
+
         for (int row = 0; row < board.length; row++) {
             for (int column = 0; column < board[0].length; column++) {
                 setValue(row, column, EMPTY);
             }
         }
     }
-    
+
     public void pass() {
         // When both players pass consecutively, end the game
         if (lastPlayerPassed) {
             gameEnded = true;
             return;
         }
-        
+
         lastPlayerPassed = true;
         move += 1;
         currentPlayer = getOtherPlayer(currentPlayer);
     }
-    
+
     public void play(int row, int column) {
         setValue(row, column, currentPlayer);
-        
+
         // Capturing opponent takes priority
         capture(row, column, getOtherPlayer(currentPlayer));
         capture(row, column, currentPlayer);
-        
+
         lastPlayerPassed = false;
         move += 1;
         currentPlayer = getOtherPlayer(currentPlayer);
     }
-    
+
     // Remove a neighboring chain if it has no liberty
     private void capture(int row, int column, int player) {
         ArrayList<ArrayList<Point>> neighborChains = getNeighborChains(row, column, player);
-        
+
+        int captured = 0;
+
         for (ArrayList<Point> chain : neighborChains) {
             if (isChainLiberal(chain) == false) {
                 for (Point point : chain) {
                     setValue(point.y, point.x, EMPTY);
+                    captured++;
                 }
             }
         }
+
+        if (player == WHITE && currentPlayer == BLACK) {
+            stonesCapturedByBlack += captured;
+        } else if (player == BLACK && currentPlayer == WHITE) {
+            stonesCapturedByWhite += captured;
+        }
     }
-    
+
     public boolean canPlay(int row, int column) {
         // P3 TODO: Check that a value was never occcupied by the same player (ko rule)
         // P3 TODO: Check for infinite looping behavior
@@ -73,59 +85,58 @@ public class Go {
         if (isInBounds(row, column) == false) {
             return false;
         }
-        
+
         if (getValue(row, column) != EMPTY) {
             return false;
         }
-        
-        if (allowSelfCapture == false && selfCapturePrecondition(row, column)) {
+
+        if (allowSelfCapture == false && preconditionSelfCapture(row, column)) {
             return false;
         }
-        
+
         return true;
     }
-    
+
     // A move can be illegal if only one's stones are removed after a play
-    private boolean selfCapturePrecondition(int row, int column) {
+    private boolean preconditionSelfCapture(int row, int column) {
         int otherPlayer = getOtherPlayer(currentPlayer);
         ArrayList<ArrayList<Point>> neighborChains = getNeighborChains(row, column, otherPlayer);
-        
+
         for (ArrayList<Point> chain : neighborChains) {
             if (isChainLiberal(chain) == false) {
                 return false;
             }
         }
-        
+
         setValue(row, column, currentPlayer);
         ArrayList<Point> currentChain = getChain(row, column);
         boolean chainLiberal = isChainLiberal(currentChain) == false;
         setValue(row, column, EMPTY);
-        
+
         return chainLiberal;
     }
-    
+
     /*
     Finds directly adjacent clusters of the same color
     This approach recursively searches immediate neighbors and avoids searching previously searched values
         - To improve speed, don't include checked indexes as neighbors
         - Track the angle that a given getChain call has differed from the origin
         - P3 TODO: Look more than one ahead, no need to get forward neighbor
-    */
+     */
     private ArrayList<Point> getChain(int row, int column) {
         ArrayList<Point> chain = new ArrayList();
-        
-        chain.add(new Point(column, row));
-        
         int targetPlayer = getValue(row, column);
-        
+
+        chain.add(new Point(column, row));
+
         getChain(chain, row, column, targetPlayer);
-        
+
         return chain;
     }
-    
+
     private void getChain(ArrayList<Point> chain, int row, int column, int player) {
         ArrayList<Point> neighbors = getNeighbors(row, column);
-        
+
         for (Point neighbor : neighbors) {
             if (getValue(neighbor.y, neighbor.x) == player && chain.contains(neighbor) == false) {
                 chain.add(new Point(neighbor.x, neighbor.y));
@@ -133,138 +144,150 @@ public class Go {
             }
         }
     }
-    
+
     private ArrayList<Point> getNeighbors(int row, int column) {
         ArrayList<Point> neighbors = new ArrayList();
-        
+
         if (isInBounds(row - 1, column)) { // North
             neighbors.add(new Point(column, row - 1));
         }
-        
+
         if (isInBounds(row, column + 1)) { // East
             neighbors.add(new Point(column + 1, row));
         }
-        
+
         if (isInBounds(row + 1, column)) { // South
             neighbors.add(new Point(column, row + 1));
         }
-        
+
         if (isInBounds(row, column - 1)) { //West
             neighbors.add(new Point(column - 1, row));
         }
-        
+
         return neighbors;
     }
-    
+
     private ArrayList<ArrayList<Point>> getNeighborChains(int row, int column, int player) {
         // P3 TODO: check that chains are not duplicated
-        
+
         ArrayList<ArrayList<Point>> neighborChains = new ArrayList();
         ArrayList<Point> stonesOfInterest = getNeighbors(row, column);
-        
+
         if (player == currentPlayer) {
             stonesOfInterest.add(new Point(column, row));
         }
-        
+
         for (Point stoneOfInterest : stonesOfInterest) {
             if (getValue(stoneOfInterest.y, stoneOfInterest.x) == player) {
                 ArrayList<Point> chain = getChain(stoneOfInterest.y, stoneOfInterest.x);
                 neighborChains.add(chain);
             }
         }
-        
+
         return neighborChains;
     }
-    
+
     public int getPlayer() {
         return currentPlayer;
     }
-    
+
     public boolean getGameEnded() {
         return gameEnded;
     }
-    
+
     public int getMove() {
         return move;
     }
-    
+
     public int getOtherPlayer(int player) {
         if (player == BLACK) {
             return WHITE;
         }
-        
+
         return BLACK;
     }
-    
-    /*
-    // P2 TODO
-    // Territory scoring counts a player's territory minus the stones captured byt he opponent
-    public int getScoreTerritoy(int player) {
-        // loop over every empty space
-        // if a space is surrounded by "player", add point to that player
-        // if a space is surrounded by multiple players, no points are given
-        // subtract captured stone count int from that score
+
+    public int getSize() {
+        return size;
     }
-    */
-    
+
     // Area scoring counts a player's territoty plus the number of stone they have
     public int getScoreArea(int player) {
-        ArrayList<Point> checkedPoints = new ArrayList();
-        int score = 0;
         int stones = 0;
-        
-        // Track territory and player stones
+
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board[0].length; x++) {
                 int value = getValue(y, x);
-                
-                if (value == EMPTY) {
-                    if (checkedPoints.contains(new Point(y, x))) {
-                        continue;
-                    }
-                    
-                    ArrayList<Point> chain = getChain(y, x);
-                    checkedPoints.addAll(chain);
-                    
-                    // If a space is surrounded by "player", add a point to that player
-                    // If a space is surrounded by multiple players, no point is given
-                    if (isChainPlayerTerritoy(chain, player)) {
-                        score += chain.size();
-                    }
-                } else if (value == player) {
+
+                if (value == player) {
                     stones++;
                 }
             }
         }
-        
-        return stones + score;
+
+        return getTerritory(player) + stones;
     }
-    
-    public int getSize() {
-        return size;
+
+    // Territory scoring counts a player's territory minus the stones captured by the opponent
+    public int getScoreTerritoy(int player) {
+        int stonesCaptured = player == BLACK
+                ? stonesCapturedByWhite
+                : stonesCapturedByBlack;
+
+        return getTerritory(player) - stonesCaptured;
     }
-    
+
+    public int getTerritory(int player) {
+        ArrayList<Point> checkedPoints = new ArrayList();
+        int territory = 0;
+
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[0].length; x++) {
+                int value = getValue(y, x);
+
+                if (value != EMPTY) {
+                    continue;
+                }
+
+                if (checkedPoints.contains(new Point(y, x))) {
+                    continue;
+                }
+
+                ArrayList<Point> chain = getChain(y, x);
+                checkedPoints.addAll(chain);
+
+                // If a space is surrounded by "player", it belongs to ""player
+                // If a space is surrounded by both players, it belongs to neither
+                if (isChainPlayerTerritoy(chain, player)) {
+                    territory += chain.size();
+                }
+            }
+        }
+
+        return territory;
+    }
+
     public int getValue(int row, int column) {
         return board[row][column];
     }
-    
+
     private boolean isChainLiberal(ArrayList<Point> chain) {
         for (Point point : chain) {
             if (isLiberal(point.y, point.x)) {
                 return true;
             }
         }
-        
+
         return false;
     }
-    
+
     // Territory is defined as empty spaces that are adjacdent with only the players stones
     private boolean isChainPlayerTerritoy(ArrayList<Point> chain, int player) {
         // Optimize with cache of checked neighbors
 
         for (Point point : chain) {
             ArrayList<Point> neighbors = getNeighbors(point.y, point.x);
-            
+
             for (Point neighbor : neighbors) {
                 int value = getValue(neighbor.y, neighbor.x);
                 if (value != player && value != EMPTY) {
@@ -272,30 +295,30 @@ public class Go {
                 }
             }
         }
-        
+
         return true;
     }
-    
+
     public boolean isInBounds(int row, int column) {
         return isInBounds(row) && isInBounds(column);
     }
-    
+
     public boolean isInBounds(int axis) {
         return axis >= 0 && axis < size;
     }
-    
+
     private boolean isLiberal(int row, int column) {
         ArrayList<Point> neighbors = getNeighbors(row, column);
-        
+
         for (Point neighbor : neighbors) {
             if (getValue(neighbor.y, neighbor.x) == EMPTY) {
                 return true;
             }
         }
-        
+
         return false;
     }
-    
+
     private void setValue(int row, int column, int value) {
         board[row][column] = value;
     }
